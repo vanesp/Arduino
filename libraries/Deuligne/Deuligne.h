9fc1a92e7f14391a47d4aef3ab1b0ca060ded52c
Peter van Es <vanes.peter@gmail.com>
2016-01-31 14:57:04 +0100

    Libraries updated and added

--- libraries/Deuligne/Deuligne.h
+++ libraries/Deuligne/Deuligne.h
@@ -16,11 +16,14 @@
 #define SNOOT_WIREWRITE Wire.send
 #endif
 
+#if ! defined(HANDLE_UTF8_CHAR)
+#define HANDLE_UTF8_CHAR 1
+#endif
 
 
 #include <inttypes.h>
 #include <Print.h>
-
+#include <Wire.h>
 
 // commands
 #define LCD_CLEARDISPLAY 0x01
@@ -65,12 +68,39 @@
 #define CPU_FREQ 16000000L // (...) - a discuter, car fonction vitesse clock cpu
 #define TWI_FREQ_MCP23008 400000L
 
-// IMPORTANT! Wire. must have a begin() before calling init()
 
+#if HANDLE_UTF8_CHAR >= 1
+typedef struct {
+	uint8_t chr[4];
+	uint8_t display[8];
+} widechar_t;
+
+static const widechar_t widechars[] =
+{
+	{ "é" , { 0b00010, 0b00100, 0b01110, 0b10001, 0b11111, 0b10000, 0b01110, 0b00000}},
+	{ "è" , { 0b01000, 0b00100, 0b01110, 0b10001, 0b11111, 0b10000, 0b01110, 0b00000}},
+	{ "ê" , { 0b00100, 0b01010, 0b01110, 0b10001, 0b11111, 0b10000, 0b01110, 0b00000}},
+	{ "ë" , { 0b01010, 0b00000, 0b01110, 0b10001, 0b11111, 0b10000, 0b01110, 0b00000}},
+	{ "à" , { 0b01000, 0b00100, 0b01110, 0b00001, 0b01111, 0b10001, 0b01111, 0b00000}},
+	{ "â" , { 0b00100, 0b01010, 0b01110, 0b00001, 0b01111, 0b10001, 0b01111, 0b00000}},
+	{ "ù" , { 0b01000, 0b00100, 0b10001, 0b10001, 0b10001, 0b10011, 0b01101, 0b00000}},
+	{ "û" , { 0b00100, 0b01010, 0b10001, 0b10001, 0b10001, 0b10011, 0b01101, 0b00000}},
+	{ "ç" , { 0b01110, 0b10000, 0b10000, 0b10001, 0b01110, 0b00100, 0b01000, 0b00000}},
+	{ "ô" , { 0b00100, 0b01010, 0b01110, 0b10001, 0b10001, 0b10001, 0b01110, 0b00000}},
+	{ "î" , { 0b00100, 0b01010, 0b00000, 0b01100, 0b00100, 0b00100, 0b01110, 0b00000}},
+	{ "ï" , { 0b01010, 0b00000, 0b01100, 0b00100, 0b00100, 0b00100, 0b01110, 0b00000}},
+	{ "£" , { 0b00110, 0b01000, 0b01000, 0b11110, 0b01000, 0b01001, 0b10110, 0b00000}},
+	{ "€" , { 0b00110, 0b01001, 0b11100, 0b01000, 0b11100, 0b01001, 0b00110, 0b00000}},
+	{ "µ" , { 0b00000, 0b00000, 0b00000, 0b10001, 0b10001, 0b11001, 0b10110, 0b10000}},
+	{ "œ" , { 0b00000, 0b00000, 0b01010, 0b10101, 0b10111, 0b10100, 0b01011, 0b00000}},
+
+	/* Please complete with the UTF-8 chars you need */
+};
+#endif
 
 class Deuligne : public Print {
 public:
-  Deuligne(int devI2CAddress=0xA7, int num_lines=2, int lcdwidth=16, int bufferwidth= 40);
+  Deuligne(uint8_t devI2CAddress=0xA7, uint8_t num_lines=2, uint8_t lcdwidth=16, uint8_t bufferwidth= 40);
   void commandWrite(int command);
   void init();
   void clear();
@@ -97,7 +127,7 @@ public:
   /**
    * from LiquidCrystal (official arduino) Library
    ***/
-  void createChar(uint8_t, uint8_t[]);
+  void createChar(uint8_t,const uint8_t[]);
   void command(uint8_t);
 
   void home();
@@ -115,25 +145,27 @@ public:
   void autoscroll();
   void noAutoscroll();
 
-  int get_key();
+  int8_t get_key();
 
 private:
   //void pulseEnablePin();
   //void pushNibble(int nibble);
   //void pushByte(int value);
+ #if HANDLE_UTF8_CHAR >= 1
+  void loadAndPrintExtendedChar(uint8_t value);
+  void handle_utf8(uint8_t value);
+#endif
 
+  uint8_t last_ddramaddr;
 
-  int myNumLines;
-  int myWidth;
-  int myAddress;
-  int myBufferwidth;
-  int adc_key_in;
-  //  int adc_key_val[5];
-  int NUM_KEYS;
+  uint8_t myNumLines;
+  uint8_t myWidth;
+  uint8_t myAddress;
+  uint8_t myBufferwidth;
 
-  uint8_t _displayfunction;
   uint8_t _displaycontrol;
   uint8_t _displaymode;
+  uint8_t utf_hi_char;
 
 
 };
